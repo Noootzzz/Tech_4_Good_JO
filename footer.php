d40d97823d20e923696879cb12492737e96e12e7
Noot <nathan.boisleux@edu.devinci.fr>
2024-03-28 10:32:52 +0100

    ajout php

---
+++ footer.php
@@ -0,0 +1,16 @@
+<footer>
+    <div class="contact">
+        <a href="contact.html">Nous contacter</a>
+    </div>
+
+    <div class="social">
+        <li><a href="#">Youtube</a></li>
+        <li><a href="#">Twitter</a></li>
+        <li><a href="#">Instagram</a></li>
+        <li><a href="#">Facebook</a></li>
+    </div>
+
+    <div class="copyright">
+        <p>© 2024 Copyright: Esco </p>
+    </div>
+</footer> 
---
+++ header.php
@@ -0,0 +1,13 @@
+<header>
+        <nav>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">News</a></li>
+                <li><a href="#">Promotion</a></li>
+                <li><img src="" alt=""></li>
+                <li><a href="#">Data</a></li>
+                <li><a href="#">Contact</a></li>
+                <li><a href="#">Connexion</a></li>
+            </ul>
+        </nav>
+    </header>

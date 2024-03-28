f8748d46ee9bca044fa2594a2fd07bcd63df7fcd
17Sx <noa.obringer@gmail.com>
2024-03-28 11:59:17 +0100

    Page contact php

--- contact.php
+++ contact.php
@@ -7,6 +7,22 @@
     <link rel="stylesheet" href="css/contact.css">
 </head>
 <body>
+
+<?php 
+
+
+    require('haut_page.php');
+    require('header.php');
+
+?>
+
+
+<h2>Contact</h2>
+<form action="traitement_contact.php" method="post">
+            <label for="contact">Nous Contactez</label>
+            <textarea placeholder="Quelle est votre probleme ?" name="content" id="content" cols="30" rows="5"></textarea>
+            <button class="btnform" type="submit">Envoyer</button>
+</form>
     
 </body>
 </html>

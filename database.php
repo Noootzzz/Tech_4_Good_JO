b35271211809a4cda0d4b30c799ef70870c6e2f5
17Sx <noa.obringer@gmail.com>
2024-03-28 09:53:23 +0100

    Creation login/register Page
    Creation de la page de login et register + modification de tout les dossier en php pour nous permettre d'utiliser une base de donnée

---
+++ database.php
@@ -0,0 +1,40 @@
+<?php
+
+//permet de ce connecter a la base de données
+
+try {
+    $database = new PDO('mysql:host=localhost;dbname=tech4good', 'root', '');
+} catch (PDOException $e) {
+    die('Site Indisponible pour le moment.');
+}
+
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['form']) && $_POST['form'] == 'ajout') {
+    if (!empty($_POST['pseudo']) && !empty($_POST['email']) && !empty($_POST['password'])) {
+        $pseudo = $_POST['pseudo'];
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+
+
+
+    $requete = $database->prepare("INSERT INTO user (pseudo, email, password) VALUES (:pseudo, :email, :password)");
+    $requete->bindParam(':pseudo', $pseudo);
+    $requete->bindParam(':email', $email);
+    $requete->bindParam(':password', $password);
+
+    // Exécute la requête
+
+
+        if ($requete->execute()) {
+            echo '<script type="text/javascript">alert("Compte crée !");</script>'; //permet de mettre du JS dans le php
+            header("Location: login.php");
+        } else {
+            echo '<script type="text/javascript">alert("Votre compte n/a pas pus etre crée !");</script>';  
+            header("Location: register.php");      }
+    } else {
+        echo '<script type="text/javascript">alert("Veuiller a remplir tout les champs SVP !");</script>';
+        header("Location: register.php"); 
+    }
+}
+
+?>
